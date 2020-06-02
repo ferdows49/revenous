1d4b242396aeccf2cb7b60f4be59fc0efca5c2e2
Ferdows Ohid <ferdows.ohid49@gmail.com>
2020-06-03 02:07:32 +0600

    modify search option

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,11 +20,17 @@ const businesses = [
 ];
 
 class App extends React.Component {
+
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`)
+  }
+
+
   render() {
     return (
       <div className='App'>
         <h1>revenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} />
       </div>
     );
